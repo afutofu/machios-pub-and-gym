7030ce9111133fce182160abcfc316208fcd1642
Afuza <thegannoz@gmail.com>
2020-03-18 12:41:52 +0700

    Added initial Menu area

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,11 +3,13 @@ import React from "react";
 import classes from "./Main.module.css";
 
 import ImageSlider from "../ImageSlider/ImageSlider";
+import Menu from "../Menu/Menu";
 
 const main = () => {
   return (
     <div className={classes.Main}>
       <ImageSlider />
+      <Menu />
     </div>
   );
 };
---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import classes from "./Menu.module.css";
+
+const menu = () => {
+  return (
+    <div className={classes.Menu}>
+      <h1>MENU</h1>
+      <h6>Available every day bruhhh</h6>
+    </div>
+  );
+};
+
+export default menu;
