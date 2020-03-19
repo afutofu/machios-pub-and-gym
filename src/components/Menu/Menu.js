8e53dedd13f9a39f8720c86fb8a98b75c9abfefb
Afuza <thegannoz@gmail.com>
2020-03-19 08:32:44 +0700

    Added SectionHeader to Menu component

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -2,11 +2,15 @@ import React from "react";
 
 import classes from "./Menu.module.css";
 
+import SectionHeader from "../SectionHeader/SectionHeader";
+
 const menu = () => {
   return (
     <div className={classes.Menu}>
-      <h1>MENU</h1>
-      <h6>Available every day bruhhh</h6>
+      <SectionHeader title="menu" />
+      <div className={classes.Container}>
+        <h6>Available every day bruhhh</h6>
+      </div>
     </div>
   );
 };
