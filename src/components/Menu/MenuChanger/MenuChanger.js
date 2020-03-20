bb4fe15a8f7e889ea9abe458a719d087e63905cb
Afuza <thegannoz@gmail.com>
2020-03-20 08:50:19 +0700

    Imrpoved styling on MenuChanger

--- src/components/Menu/MenuChanger/MenuChanger.js
+++ src/components/Menu/MenuChanger/MenuChanger.js
@@ -6,8 +6,8 @@ const menuChanger = () => {
   return (
     <div className={classes.MenuChanger}>
       <div className={classes.Options}>
-        <p className={classes.Meals}>MEALS</p>
         <p className={classes.Drinks}>DRINKS</p>
+        <p className={classes.Meals}>MEALS</p>
       </div>
     </div>
   );
