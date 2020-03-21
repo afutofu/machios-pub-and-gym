150f2ca7848faded49505bd4edda145664ac09b3
Afuza <thegannoz@gmail.com>
2020-03-21 10:14:56 +0700

    Fixed bug with image in ImageSlider

--- src/components/ImageSlider/ImageSlider.js
+++ src/components/ImageSlider/ImageSlider.js
@@ -2,8 +2,6 @@ import React from "react";
 
 import classes from "./ImageSlider.module.css";
 
-import luxurybar1 from "../../assets/backgrounds/luxurybar1.jpg";
-
 const imageSlider = () => {
   return (
     <div id="home" className={classes.ImageSlider}>
