5fd4ece2596d9898b9dc6d63e9d5c67d54ac1e7c
Afuza <thegannoz@gmail.com>
2020-03-24 13:38:18 +0700

    Added more content to Contact component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -8,6 +8,27 @@ const contact = () => {
   return (
     <section id="contact" className={classes.Contact}>
       <SectionHeader title="contact" />
+      <div className={classes.Container}>
+        <p className={classes.Title}>FIND US</p>
+        <div className={classes.Close}>
+          <p>104 Diamond Block, Top Floor,</p>
+          <p>Burj Khalifah, B9 BE8</p>
+        </div>
+        <hr />
+        <p className={classes.Title}>RESERVATION</p>
+        <div className={classes.Close}>
+          <p>123.123.1234 or Book online here</p>
+          <p>For any other information</p>
+          <p>ask@machospubngym.com</p>
+        </div>
+        <hr />
+        <p className={classes.Title}>SPECIAL OCCASIONS</p>
+        <div className={classes.Close}>
+          <p>All items on 50% discount on Black Friday</p>
+          <p>Items containing eggs are 50% discount on Easter</p>
+          <p></p>
+        </div>
+      </div>
     </section>
   );
 };
--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -5,7 +5,7 @@ import classes from "./Gallery.module.css";
 const gallery = () => {
   return (
     <section id="gallery" className={classes.Gallery}>
-      Test
+      GALLLERY
     </section>
   );
 };
