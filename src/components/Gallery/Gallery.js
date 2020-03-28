c534cc3e44c75eb4c9b0e3307d9081f79f81d458
Afuza <thegannoz@gmail.com>
2020-03-28 19:28:50 +0700

    Implemented initial gallery section

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -2,10 +2,29 @@ import React from "react";
 
 import classes from "./Gallery.module.css";
 
+import pub1 from "../../assets/images/pub2.jpg";
+import gym1 from "../../assets/images/gym3.jpg";
+
 const gallery = () => {
   return (
     <section id="gallery" className={classes.Gallery}>
-      GALLLERY
+      <div className={classes.Slides}>
+        <div className={classes.Slide}>
+          <div className={classes.Image} />
+        </div>
+        <div className={classes.Slide}>
+          <div className={classes.Image} />
+        </div>
+        <div className={classes.Slide}>
+          <div className={classes.Image} />
+        </div>
+        <div className={classes.Slide}>
+          <div className={classes.Image} />
+        </div>
+        <div className={classes.Slide}>
+          <div className={classes.Image} />
+        </div>
+      </div>
     </section>
   );
 };
