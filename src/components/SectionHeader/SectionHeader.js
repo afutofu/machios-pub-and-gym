461bb15975905176657295274594103bcaf46c52
Afuza <thegannoz@gmail.com>
2020-04-01 13:45:08 +0700

    Implemented animation on Section Header
    Animation starts when component is on screen

--- src/components/SectionHeader/SectionHeader.js
+++ src/components/SectionHeader/SectionHeader.js
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 
 import classes from "./SectionHeader.module.css";
 
-const sectionHeader = props => {
-  return <div className={classes.SectionHeader}>{props.title}</div>;
-};
+class sectionHeader extends Component {
+  myRef = React.createRef();
+
+  state = { yPosition: 0, onScreen: false };
+
+  componentDidMount() {
+    this.setState({ yPosition: this.myRef.current.getBoundingClientRect().y });
+    this.interval = setInterval(() => {
+      if (
+        this.state.yPosition <
+        window.pageYOffset + window.screen.height * 0.6
+      ) {
+        this.setState({ onScreen: true });
+      } else {
+        this.setState({ onScreen: false });
+      }
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  renderContent = () => {
+    {
+      if (this.state.onScreen) {
+        return (
+          <div className={classes.SlideInSectionHeader} ref={this.myRef}>
+            {this.props.title}
+          </div>
+        );
+      } else {
+        return (
+          <div className={classes.SlideOutSectionHeader} ref={this.myRef}>
+            {this.props.title}
+          </div>
+        );
+      }
+    }
+  };
+
+  render() {
+    return this.renderContent();
+  }
+}
 
 export default sectionHeader;
