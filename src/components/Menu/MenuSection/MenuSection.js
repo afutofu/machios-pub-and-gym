fc4eea7ff2d0bdfd20268d7474e014bab122be25
Afuza <thegannoz@gmail.com>
2020-04-02 12:42:58 +0700

    Animated MenuSection
    MenuSectionTitle fades in, hr slides in from the left

--- src/components/Menu/MenuSection/MenuSection.js
+++ src/components/Menu/MenuSection/MenuSection.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 
 import classes from "./MenuSection.module.css";
 
 import MenuItem from "../MenuItem/MenuItem";
 
-const menuSection = props => {
-  const renderMenuItems = () => {
+class menuSection extends Component {
+  myRef = React.createRef();
+
+  state = { yPosition: 0, onScreen: false };
+
+  componentDidMount() {
+    this.setState({ yPosition: this.myRef.current.getBoundingClientRect().y });
+
+    this.interval = setInterval(() => {
+      if (
+        window.pageYOffset + window.screen.height - window.screen.height * 0.4 >
+        this.state.yPosition
+      ) {
+        this.setState({ onScreen: true });
+      } else if (
+        window.pageYOffset + window.screen.height - window.screen.height * 0.1 <
+        this.state.yPosition
+      ) {
+        this.setState({ onScreen: false });
+      }
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  renderMenuItems = () => {
     let menuItems = [];
-    for (let i = 0; i < props.items; i++) {
+    for (let i = 0; i < this.props.items; i++) {
       menuItems.push(
         <MenuItem
           key={i}
@@ -20,13 +46,33 @@ const menuSection = props => {
     return menuItems;
   };
 
-  return (
-    <div className={classes.MenuSection}>
-      <p className={classes.MenuSectionTitle}>{props.title}</p>
-      <hr />
-      <div className={classes.MenuItems}>{renderMenuItems()}</div>
-    </div>
-  );
-};
+  renderContent = () => {
+    let menuSectionTitleClasses = [classes.MenuSectionTitle, ""];
+    let hrClasses = [""];
+
+    menuSectionTitleClasses.pop();
+    hrClasses.pop();
+
+    if (this.state.onScreen) {
+      menuSectionTitleClasses.push(classes.FadeIn);
+      hrClasses.push(classes.SlideIn);
+    } else {
+      menuSectionTitleClasses.push("");
+      hrClasses.push("");
+    }
+
+    return (
+      <div className={classes.MenuSection} ref={this.myRef}>
+        <p className={menuSectionTitleClasses.join(" ")}>{this.props.title}</p>
+        <hr className={hrClasses} />
+        <div className={classes.MenuItems}>{this.renderMenuItems()}</div>
+      </div>
+    );
+  };
+
+  render() {
+    return this.renderContent();
+  }
+}
 
 export default menuSection;
