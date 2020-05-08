d988cf0aebc4c85e1b894138f62921487c464c41
Afuza <thegannoz@gmail.com>
2020-05-08 14:42:16 +0700

    Made working automatic ImageSlider

--- src/components/ImageSlider/ImageSlider.js
+++ src/components/ImageSlider/ImageSlider.js
@@ -1,24 +1,51 @@
 import React from "react";
 
 import bar1 from "../../assets/images/luxurybar1.jpg";
-import bar2 from "../../assets/images/luxurybar1.jpg";
-import bar3 from "../../assets/images/luxurybar1.jpg";
+import bar2 from "../../assets/images/luxurybar2.jpg";
+import bar3 from "../../assets/images/luxurybar3.jpg";
 
 import classes from "./ImageSlider.module.css";
 
 class imageSlider extends React.Component {
-  constructor(props) {
-    super(props);
+  state = { imageIndex: 0, images: [bar1, bar2, bar3] };
 
-    this.state = { images: [bar1, bar2, bar3] };
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.slideImages();
+    }, 7000);
   }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  slideImages = () => {
+    const { imageIndex, images } = this.state;
+    if (imageIndex === images.length - 1) {
+      this.setState({ imageIndex: 0 });
+    } else {
+      this.setState({ imageIndex: imageIndex + 1 });
+    }
+  };
 
   render() {
     return (
       <section id="home" className={classes.ImageSlider}>
-        <div className={classes.Images}>
-          <div className={classes.Transparent} />
+        <div
+          className={classes.Slider}
+          style={{ left: `-${this.state.imageIndex}00%` }}
+        >
+          {this.state.images.map((imageLink) => {
+            return (
+              <img
+                src={imageLink}
+                alt="failed to load"
+                className={classes.Image}
+              />
+            );
+          })}
         </div>
+        <div className={classes.Transparent} />
       </section>
     );
   }
